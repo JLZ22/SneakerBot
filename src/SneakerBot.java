f923ff92258ad9de298d2f9a1846fe1d548b272d
JLZ22 <johnzeng878@gmail.com>
2022-12-08 07:53:58 -0500

    extractFormData method

--- src/SneakerBot.java
+++ src/SneakerBot.java
@@ -1,10 +1,12 @@
 import java.io.*;
 import java.net.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SneakerBot {
     public static void main(String[] args) throws IOException, InterruptedException {
         // The URL of the sneaker release page
-        URL url = new URL("https://www.sneakerstore.com/releases");
+        URL url = new URL("https://www.nike.com/w/new-mens-3n82yznik1");
 
         // Check the release page every minute
         while (true) {
@@ -22,7 +24,7 @@ public class SneakerBot {
             String response = in.readLine();
 
             // Check if the sneaker is released
-            if (response.contains("Sneaker Name") &&
+            if (response.contains("Air Jordan 12 Retro") &&
                     response.contains("In Stock")) {
                 // The sneaker is released and in stock, so buy it!
                 buySneaker();
@@ -35,7 +37,7 @@ public class SneakerBot {
 
     public static void buySneaker() throws IOException {
         // The URL of the sneaker page
-        URL url = new URL("https://www.nike.com/sneaker/123456");
+        URL url = new URL("https://www.nike.com/w/new-mens-3n82yznik1");
 
         // Open a connection to the sneaker page
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -81,8 +83,31 @@ public class SneakerBot {
         }
     }
 
-    public static String extractFormData(String html) {
-        // TODO: Implement this method to extract the form data from the HTML
-        return null;
+    public static String extractFormData(String html) throws UnsupportedEncodingException {
+        // The form data string
+        StringBuilder formData = new StringBuilder();
+
+        // Use a regular expression to find the form fields in the HTML
+        Pattern pattern = Pattern.compile("<input[^>]*name=\"([^\"]*)\"[^>]*value=\"([^\"]*)\"[^>]*>");
+        Matcher matcher = pattern.matcher(html);
+
+        // Iterate over the form fields
+        while (matcher.find()) {
+            // Get the name and value of the form field
+            String name = matcher.group(1);
+            String value = matcher.group(2);
+
+            // Append the name and value to the form data string
+            formData.append(name);
+            formData.append("=");
+            formData.append(URLEncoder.encode(value, "UTF-8"));
+            formData.append("&");
+        }
+
+        // Remove the trailing '&' character from the form data string
+        formData.setLength(formData.length() - 1);
+
+        // Return the form data string
+        return formData.toString();
     }
 }
